aea6aa276a349b25a7e4b9acad1cd9ed538d77b7
akash-indulkar <akashindulkar6677@gmail.com>
2024-08-01 15:09:48 +0530

    API Gateway for both the Stock Exchange and StockPrice microservice is created

---
+++ src/main/java/com/akashxdev/ApiGatewayForStockExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.akashxdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayForStockExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayForStockExchangeApplication.class, args);
+	}
+
+}
